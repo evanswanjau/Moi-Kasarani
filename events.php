e57c07974aecc93422b0dfaaa2920fa20a91dd0b
Evans Wanjau <evanswanjau@gmail.com>
2017-02-24 11:16:02 +0300

    Made backend on the register page

---
+++ events.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<!--Beginning of the tickets header-->
+  <head>
+    <meta charset="utf-8">
+    <title>Tickets | Moi Kasarani Ticket Booking System</title>
+    <!--Link for CSS styling-->
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <div class="events container-fluid">
+      <div class="row">
+
+      </div>
+    </div>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<!--This is the header for registration page-->
+  <head>
+    <meta charset="utf-8">
+    <title>Register | Moi Kasarani Ticket Booking System</title>
+    <!--Link for CSS styling-->
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <!--Here's the registration form-->
+    <form class="register" action="index.html" method="post">
+      email:<input type="text" name="email">
+      username<input type="email" name="email">
+      password<input type="password" name="password">
+      Confirm password<input type="password" name="password2">
+      <input type="button" name="submit" value="Submit">
+    </form>
+  </body>
+</html>
